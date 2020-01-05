2fed4ea66fc75ff19fcbc713ec8a56a9b971078e
iberatkaya <beratkaya1998@gmail.com>
2020-01-05 21:01:04 +0300

    Updates

---
+++ precache-manifest.51fc9b109f372f2dceecd2430d79f597.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0eeebe3e545c57e7eb3bf95ec81c569",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a219c8df074d55674e63",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bd5de610a5f6f5282459",
+    "url": "./static/css/main.2ed57727.chunk.css"
+  },
+  {
+    "revision": "a219c8df074d55674e63",
+    "url": "./static/js/2.25a9dfa1.chunk.js"
+  },
+  {
+    "revision": "f7c631b643422c9fb8f223014aae8b68",
+    "url": "./static/js/2.25a9dfa1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd5de610a5f6f5282459",
+    "url": "./static/js/main.6fe2e69d.chunk.js"
+  },
+  {
+    "revision": "e79acb52121e7d6dd478",
+    "url": "./static/js/runtime-main.c7a21b67.js"
+  }
+]);
